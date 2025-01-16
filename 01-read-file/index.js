fdf2f3e0c20f82182b62a995b8a8b89d35edd49f
Viktoria Kondrashova <kavidmi@gmail.com>
2025-01-16 12:02:35 +0300

    fix: fix first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,6 +2,5 @@ const fs = require('fs');
 const path = require('path');
 
 const dir = path.join(__dirname, 'text.txt');
-const stream = new fs.ReadStream(dir, { encoding: 'utf-8' });
-
-stream.on('readable', () => console.log(stream.read()));
+const stream = fs.createReadStream(dir, 'utf-8');
+stream.on('data', (chunk) => console.log(chunk));
